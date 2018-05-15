fa392cd918fc4d0e6b8f9ec4d19396e1608e4d1f
rajarshikhare <rajarshi.vaibhav@gmail.com>
2018-05-15 17:18:27 +0530

    added setting to use postgress

--- myblog/settings.py
+++ myblog/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
     }
 }
 
+#mysetting for postgress
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
