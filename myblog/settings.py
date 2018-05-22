cbd0926ed52338724b0bd792e79b49ba2da0dd7c
rajarshikhare <rajarshi.vaibhav@gmail.com>
2018-05-22 17:46:44 +0530

    --

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%ot0*w1g#33*7fs(=l^%u_jm71muy&=da1s)9b!i-=y!20r$^g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
